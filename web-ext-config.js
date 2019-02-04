7c70d4b561973dd2f1b82170f0d2a04e18b2eeea
Anthony Frehner <anthony.frehner@canopytax.com>
2019-02-04 15:55:19 -0700

    config updates and other touch-ups

--- web-ext-config.js
+++ web-ext-config.js
@@ -1,4 +1,13 @@
 // config file for web-ext
 module.exports = {
-  ignoreFiles: ["src/**", "node_modules/**", ".git/**"]
+  ignoreFiles: [
+    "src/**",
+    "node_modules/**",
+    ".git/**",
+    "package.json",
+    "package-lock.json",
+    "README.md",
+    "webpack.config.js",
+    ".nvmrc"
+  ]
 };
