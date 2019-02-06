ce5fe0b274dd34b8a62bacf1bcf7cea6dc942ac9
Anthony Frehner <anthony.frehner@canopytax.com>
2019-02-06 09:19:34 -0700

    and just like that all the packages were updated.
    removed the chrome hack since it's fixed now.

--- src/inspected-window.helper.js
+++ src/inspected-window.helper.js
@@ -10,12 +10,5 @@ export async function evalDevtoolsCmd(evalString) {
       `evalDevtoolsCmd '${evalString}' failed: ${JSON.stringify(result[1])}`
     );
   }
-
-  if (typeof InstallTrigger !== "undefined") {
-    // use this hack until this commit https://github.com/mozilla/webextension-polyfill/commit/6f178c56daed2f548d29e879e3caf082183eed3a is released
-    // see https://github.com/mozilla/webextension-polyfill/issues/168 and https://github.com/mozilla/webextension-polyfill/pull/175 and https://stackoverflow.com/a/41820692/2098017
-    return result[0];
-  } else {
-    return result;
-  }
+  return result[0];
 }
