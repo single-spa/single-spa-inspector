f9502b6f65d94276c7c2fbb89df8ffdacaa604f6
Justin McMurdie <mcmurdiej@gmail.com>
2019-02-07 14:32:28 -0700

    Kent's feedback - Update mis-named variables

--- src/inspected-window.helper.js
+++ src/inspected-window.helper.js
@@ -1,16 +1,16 @@
 import browser from "webextension-polyfill";
 
-export async function evalDevtoolsCmd(evalString) {
-  const commandString = `window.__SINGLE_SPA_DEVTOOLS__.${evalString}`;
+export async function evalDevtoolsCmd(devToolsCommandString) {
+  const commandString = `window.__SINGLE_SPA_DEVTOOLS__.${devToolsCommandString}`;
   return evalCmd(commandString);
 }
 
-export async function evalCmd(evalString) {
+export async function evalCmd(commandString) {
   // we don't try/catch here because we want the implementer to handle it... I think?
-  const result = await browser.devtools.inspectedWindow.eval(evalString);
+  const result = await browser.devtools.inspectedWindow.eval(commandString);
   if (result[1] && (result[1].isError || result[1].isException)) {
     throw new Error(
-      `evalCmd '${evalString}' failed: ${JSON.stringify(result[1])}`
+      `evalCmd '${commandString}' failed: ${JSON.stringify(result[1])}`
     );
   }
   return result[0];
