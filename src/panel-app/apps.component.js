d2297957c92ceddda5472e7fdc103de2c1b9fcb2
Anthony Frehner <anthony.frehner@canopytax.com>
2019-02-19 12:23:26 -0700

    renaming and reorganizing for better readability

--- src/panel-app/apps.component.js
+++ src/panel-app/apps.component.js
@@ -80,15 +80,15 @@ function sortApps(apps) {
 
 function highlightApp(app) {
   if (app.status !== "SKIP_BECAUSE_BROKEN" && app.status !== "NOT_LOADED") {
-    evalDevtoolsCmd(`highlight('${app.name}')`).catch(err => {
-      console.error(`Error highlighting applicaton: ${app.name}`, err);
+    evalDevtoolsCmd(`overlay('${app.name}')`).catch(err => {
+      console.error(`Error overlaying applicaton: ${app.name}`, err);
     });
   }
 }
 
 function dehighlightApp(app) {
-  evalDevtoolsCmd(`removeHighlight('${app.name}')`).catch(err => {
-    console.error(`Error De-highlighting applicaton: ${app.name}`, err);
+  evalDevtoolsCmd(`removeOverlay('${app.name}')`).catch(err => {
+    console.error(`Error removing overlay on applicaton: ${app.name}`, err);
   });
 }
 
