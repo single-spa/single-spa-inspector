2f7a78abf29be88827590bb40c96b2e7a83349a8
Carlos Filoteo <c.filoteo12@gmail.com>
2019-06-15 21:16:28 -0600

    Add Button component

---
+++ src/panel-app/button.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { useCss } from "kremling";
+
+export default function Button({ children, ...props }) {
+  const scope = useCss(css);
+  return (
+    <button {...props} {...scope} className="button">
+      {children}
+    </button>
+  );
+}
+
+const css = `
+& .button {
+  background: var(--blue5);
+  border: none;
+  border-radius: 3px;
+  color: #fff;
+  font-size: .75rem;
+  padding: .125rem .5rem;
+  text-shadow: 0px 2px 4px rgba(0,0,0,.15);
+  transition: all .15s ease-in-out;
+}
+& .button:hover,
+& .button:focus {
+  background: var(--blue7);
+}
+& .button:not(:first-of-type) {
+  margin-left: .25rem;
+}
+& .button:disabled {
+  background: var(--blue3);
+}
+`;
