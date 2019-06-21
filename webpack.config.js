ea4daaa1b903f945edb7aba15048dddd26bb8567
Anthony F <afrehner.work@gmail.com>
2019-06-21 09:16:45 -0600

    updated dependencies and changed config for those deps that had breaking changes

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 const path = require("path");
-const CleanWebpackPlugin = require("clean-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
@@ -11,8 +11,8 @@ module.exports = {
     panelApp: "./src/panel-app.js"
   },
   plugins: [
-    new CleanWebpackPlugin(["build"]),
-    CopyWebpackPlugin([
+    new CleanWebpackPlugin(),
+    new CopyWebpackPlugin([
       { from: path.resolve(__dirname, "src/panel.html") },
       { from: path.resolve(__dirname, "src/main.html") }
     ])
