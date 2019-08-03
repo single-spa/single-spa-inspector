f9c9602483d2497841028ae55a4ee6a29b130cf7
Anthony F <afrehner.work@gmail.com>
2019-08-03 16:20:24 -0600

    upgraded deps, and got the fix in for the latest cleanwebpackplugin
    also did npm audit fix, which I believe only upgraded lodash in the package-lock file

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 const path = require("path");
-const CleanWebpackPlugin = require("clean-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
@@ -11,7 +11,9 @@ module.exports = {
     panelApp: "./src/panel-app.js"
   },
   plugins: [
-    new CleanWebpackPlugin(["build"]),
+    new CleanWebpackPlugin({
+      cleanStaleWebpackAssets: false
+    }),
     new CopyWebpackPlugin([
       { from: path.resolve(__dirname, "src/panel.html") },
       { from: path.resolve(__dirname, "src/main.html") }
