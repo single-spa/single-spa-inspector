a152cfb48b2edfd9c7bb7bdc7e31f304030d2707
Carlos Filoteo <c.filoteo12@gmail.com>
2019-08-31 16:05:58 -0600

    Update ToggleGroup styling

--- src/panel-app/toggle-group.js
+++ src/panel-app/toggle-group.js
@@ -1,11 +1,13 @@
 import React from "react";
+import { useCss } from "kremling";
 import ToggleOption from "./toggle-option";
 
 export default function ToggleGroup(props) {
   const { name, onChange, value, children, ...rest } = props;
+  const styles = useCss(css);
 
   return (
-    <fieldset {...rest}>
+    <fieldset {...styles} {...rest}>
       {React.Children.map(children, child => {
         return child.type === ToggleOption
           ? React.cloneElement(child, {
@@ -18,3 +20,19 @@ export default function ToggleGroup(props) {
     </fieldset>
   );
 }
+
+const css = `
+& fieldset {
+  align-items: center;
+  border: none;
+  display: flex;
+  margin: 0;
+  padding: var(--table-spacing);
+}
+& legend {
+  color: var(--gray);
+  float: left;
+  font-size: .9rem;
+  padding-right: 1rem;
+}
+`;
