0c2caaea0c73fe131a0f2d9726b7e5f59f6d03b6
Joel Denning <joeldenning@gmail.com>
2021-11-17 16:57:53 -0700

    Slightly improve profiler css. Add download link to profiler. (#68)

--- src/profiler/profiler.js
+++ src/profiler/profiler.js
@@ -11,7 +11,7 @@ export default function Profiler() {
   useEffect(() => {
     evalDevtoolsCmd("exposedMethods.getProfilerData()").then(evts => {
       evts.sort((first, second) => second.end - first.end)
-      setProfileEvents(evts)
+      setProfileEvents(evts.sort((first, second) => second.start - first.start))
     }).catch(err => {
       console.error(err)
       setHasProfiler(false)
@@ -29,19 +29,27 @@ export default function Profiler() {
   }
 
   return (
-    <>
-      <div className="actions">
-        <Button onClick={() => setGetEvents(!getEvents)}>Refresh</Button>
-      </div>
-      <table {...scope} className="profile-table">
+    <div {...scope}>
+      <header className="profiler-header">
+        <p className="explanation">
+          The single-spa profiler shows how long microfrontends take to load, mount, unmount, etc. The timestamps come
+          from <a target="_blank" href="https://developer.mozilla.org/en-US/docs/Web/API/Performance/now">performance.now()</a>, which measures
+          the number of milliseconds since the page loaded. The profiler events are ordered from most recent to least recent.
+        </p>
+        <div className="actions">
+          <a href={csvUrl()} download="single-spa-profiling.csv">Download</a>
+          <Button onClick={() => setGetEvents(!getEvents)}>Refresh</Button>
+        </div>
+      </header>
+      <table className="profile-table">
         <thead>
           <tr>
             <th>Event Type</th>
             <th>Name</th>
             <th>Kind</th>
             <th>Succeeded</th>
-            <th>Start</th>
-            <th>Duration</th>
+            <th>Start (ms)</th>
+            <th>Duration (ms)</th>
           </tr>
         </thead>
         <tbody>
@@ -57,13 +65,31 @@ export default function Profiler() {
           ))}
         </tbody>
       </table>
-    </>
+    </div>
   );
+
+  function csvUrl() {
+    const rows = [["Event Type", "Name", "Kind", "Succeeded", "Start", "Duration"], ...profileEvents.map(evt => ([evt.type, evt.name, evt.kind, evt.operationSucceeded, evt.start, evt.end - evt.start]))]
+    const csvReport = `data:text/csv;charset=utf-8,${rows.map(r => r.join(",")).join("\n")}`
+    return encodeURI(csvReport)
+  }
 }
 
 const css = `
 .profile-table {
   width: 100%;
+  border-spacing: 0;
+}
+
+.profile-table th {
+  background-color: black;
+  color: white;
+  padding: 5px 0;
+}
+
+.profile-table td {
+  border: 1px solid black;
+  padding: 2px 0;
 }
 
 .profile-table th, .profile-table td {
@@ -72,5 +98,21 @@ const css = `
 
 .actions {
   margin: 0 32px 16px 32px;
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+}
+
+.actions *:not(last-child) {
+  margin-right: 12px;
+}
+
+.explanation {
+  margin: 0 32px 12px 32px;
+  font-size: 12px;
+}
+
+.profiler-header {
+  display: flex;
 }
 `
